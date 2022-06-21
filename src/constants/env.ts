553823c57603bd008d75eb2647b402890748eb02
mvn-sonnguyen4-hn <son.nguyen4@monstar-lab.com>
2022-06-21 21:41:05 +0700

    change port

--- src/constants/env.ts
+++ src/constants/env.ts
@@ -3,7 +3,7 @@ import packageJson from '../../package.json';
 export const ENV = {
   VERSION: packageJson.version || '',
   NODE_ENV: process.env.NODE_ENV,
-  API_HOST: 'http://35.89.85.74/api/',
+  API_HOST: 'http://35.89.85.74:4000/api/',
   URL_IMAGE_DEFAULT:
     'https://res.cloudinary.com/monstarlab777/image/upload/v1651456550/1-13_x7rcqz.png',
   API_KEY_FIREBASE: 'AIzaSyATQsqjwLx6sl65VhsDTXE3nirmiqU8_gw',
@@ -15,5 +15,5 @@ export const ENV = {
   MESSAGING_SENDER_ID: '606100025440',
   APP_ID: '1:606100025440:web:098d416f21356e5b9dd305',
   MEASUREMENT_ID: 'G-FXQ89MQQY3',
-  HOST_SOCKET: 'http://35.89.85.74/:5000/'
+  HOST_SOCKET: 'http://35.89.85.74:5000/'
 };
